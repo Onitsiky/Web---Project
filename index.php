6bad20831ad2b4a3e516060d456649a2998ad34d
Onitsiky <hei.onitsiky@gmail.com>
2022-03-08 12:00:12 +0300

    Adding new files

---
+++ index.php
@@ -0,0 +1,299 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="fr">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HEI - Haute Ecole d'Informatique</title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/jquery.fancybox.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<!-- Header Section -->
+<section class="banner" role="banner"> 
+  <!-- Banner Section -->
+  <div class="container">
+    <div class="col-md-7 col-sm-4 col-xs-11 banner-inner-wrapper">
+      <div class="banner-text">
+        <h1>HAUTE ECOLE D'INFORMATIQUE</h1>
+        <p>Professionnels du monde entier,unis pour former les étudiants Malgaches suivant les plus hauts standards.</p>
+        <a href="#formation" class="btn">En savoir plus</a> </div>
+      <!-- banner text --> 
+    </div>
+  </div>
+  <!-- Banner Section --> 
+ <!-- Navigation Section -->
+ <header id="header">
+  <div class="header-content clearfix"> <a class="logo" href="#"><img src="images/hei-logo-color-transparent.png" alt=""></a>
+    <nav class="navigation">
+      <ul class="primary-nav">
+        <li><a href="#accueil">Accueil</a></li>
+        <li><a href="#formation">Formation</a></li>
+        <li><a href="#concours">Concours</a></li>
+        <li><a href="#contact">Contact </a></li>
+      </ul>
+    </nav>
+    <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+</header>
+<!-- Navigation Section -->
+</section>
+<!-- Header Section --> 
+<!-- Section Accueil -->
+
+<section id="accueil" class="section introduction">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6">
+        <div class="intro-content">
+          <h1>Haute Ecole d'Informatique</h1>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-6">
+        <div class="intro-content">
+          <p>La Haute Ecole d'Informatique (HEI) est un établissement privé délivrant un diplôme de Bac+3 en Informatique.</p>
+          <p>La Haute Ecole d'Informatique est une école supérieure qui a pour ambition de proposer une offre de formation en adéquation avec les attentes du marché de travail actuel et des mutations digitales de la société.C'est aussi une forte équipe d'enseignants spécialisés et de partenaires d'insertion professionnels locaux et internationaux.</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class="intro-content">
+          <h4>Nos valeurs</h4>
+          <ul>
+            <li> - Entraide</li>
+            <li> - Autonomie</li>
+            <li> - Performance</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ici-->
+  <br> <br>
+  <div class="container text-center">
+    <div class="row no-gutter">
+      <div class="col-lg-3 col-xs-11">
+        <br><br><br><br><br><h2 class=" dirigeant hei">Nos dirigeants</h2>
+      </div>
+      <!-- Lou --> 
+      <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Lou.jpg" class="work-box"> <img src="image_teacher/Lou.png" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- Lou --> 
+        <!-- Ryan --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Ryan.jpg" class="work-box"> <img src="image_teacher/Ryan.png" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- Ryan --> 
+        <!-- Lyvah --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Lyvah.jpg" class="work-box"> <img src="image_teacher/Lyvah.png" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><i class="fa fa-search-plus fa-2x"></i></p>
+          </div>
+        </div>
+        <!-- Lyvah --> 
+        </a> </div>
+      </div> </div>
+  <!--ici-->
+</section>
+<!-- Section accueil --> 
+
+<!-- work section -->
+<section id="formation" class="works section no-padding">
+  <h2 class="text-center parcours">Parcours </h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-sm-4 col-xs-">
+  <h4>Deux parcours au choix :</h4>
+        <ul>
+          <li> - Ecosystème Logiciel(EL)</li>
+          <li> - Transformation Numériques (TN)</li>
+        </ul>
+        <br>
+        <h4>Diplômes délivrés :</h4>
+        <ul>
+          <li> - Licence en Ecosystème Logiciel (EL)</li>
+          <li> - Licence en Transformation Numérique (TN)</li>
+        </ul>
+      </div>
+      <div  class="col-lg-4"> <br>
+        <h4>Le parcours EL mène aux débouchés suivants :</h4>
+        <ul>
+          <li> - Développeur backend</li>
+          <li> - Développeur frontend</li>
+          <li> - Développeur mobile</li>
+          <li> - Analyste de données</li>
+          <li> - Chef de projet en informatique en EL
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-4"> <br> 
+        <h4>Le parcours TN mène aux débouchés suivants :</h4>
+        <ul>
+          <li> - Référenceur web</li>
+          <li> - Commerçant marketeur digital</li>
+          <li> - Analyste de données</li>
+          <li> - Chef de projet informatique en TN</li>
+        </ul>
+      </div>
+    </div>
+  </div> <br> <br>
+  <!-- Parcours -->
+   <!-- Méthode de Formation -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7 col-sm-5 col-xs-10">
+          <h4 class="parcours">Méthode de formation</h4>
+          <p>Notre formation se base sur deux méthodes complémentaires :</p>
+          <ul>
+            <li> > Modules techniques en informatiques selon le parcours choisi : Les cours théoriques à distance (25%) + cours en présentiel(25%).</li>
+            <li> > Apprentissage en entreprise (50%): application pratique suivant un contrat d'alternance.</li>
+          </ul>
+          <br>
+          <p>Chez HEI, les étudiants n'ont pas à copier des leçons car chaque semaine, ils recevront des vidéos concernant chaque thème abordé en classe.</p>
+      </div>
+      <div class="col-lg-5 col-sm-5 col-xs-10">
+        <h4 class="parcours">Partenariat</h4>
+        <p>HEI travaille en étroite collaboration avec l'Alliance Française notamment pour le perfectionnnement en français professionnel.</p>
+        <br> <p>Actuellement, 5 entreprises françaises sont prêtes à accueillier les étudiants à partir de la 2 ème année. Ce partenariat leur permettra de ne pas payer de frais de scolarités.</p>
+      </div>
+    </div>
+  </div> <br>
+  <!-- Méthode de Formation -->
+  <!-- Toky --> 
+  <h2 class="text-lg-center col-xs-11 parcours text-center">Notre corps enseignant</h2> <br> 
+    <div class="container-fluid">
+      <div class="row no-gutter">
+  <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Toky.jpg" class="work-box"> <img src="image_teacher/Toky.png" alt="">
+    <div class="overlay">
+      <div class="overlay-caption">
+        <p><i class="fa fa-search-plus fa-2x"></i></p>
+      </div>
+    </div>
+    <!-- Toky --> 
+    <!-- Tahina --> 
+    </a> </div>
+  <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Tahina.jpg" class="work-box"> <img src="image_teacher/Tahina.png" alt="">
+    <div class="overlay">
+      <div class="overlay-caption">
+        <p><i class="fa fa-search-plus fa-2x"></i></p>
+      </div>
+    </div>
+    <!-- Tahina -->  
+    <!-- Parison -->
+    </a> </div>
+  <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Parison.png" class="work-box"> <img src="image_teacher/Parison.png" alt="">
+    <div class="overlay">
+      <div class="overlay-caption">
+        <p><i class="fa fa-search-plus fa-2x"></i></p>
+      </div>
+    </div>
+     <!-- Parison --> 
+     <!-- Harry -->
+    </a> </div>
+  <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Harry.png" class="work-box"> <img src="image_teacher/Harry.png" alt="">
+    <div class="overlay">
+      <div class="overlay-caption">
+        <p><i class="fa fa-search-plus fa-2x"></i></p>
+      </div>
+    </div>
+   <!-- Harry -->
+   <!-- Sandrine -->
+    </a> </div>
+  <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Sandrine.jpg" class="work-box"> <img src="image_teacher/Sandrine.png" alt="">
+    <div class="overlay">
+      <div class="overlay-caption">
+        <p><i class="fa fa-search-plus fa-2x"></i></p>
+      </div>
+    </div>
+    <!-- Sandrine -->
+    <!-- Yannick -->
+    </a> </div>
+  <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12 work"> <a href="images_teacher/Yannick.jpg" class="work-box"> <img src="image_teacher/Yannick.png" alt="">
+    <div class="overlay">
+      <div class="overlay-caption">
+        <p><i class="fa fa-search-plus fa-2x"></i></p>
+      </div>
+    </div>
+     <!-- Yannick --> 
+    </a> </div>
+</div>
+</div> 
+</section><br> <br>
+<!-- Section Formation --> 
+<!-- Concours -->
+<section id="concours">
+<div class="container">
+<div class="row">
+  <div class="col-lg-6 col-md-4 col-sm-4">
+    <h4 class="parcours">Les concours sont ouverts !</h4>
+<p>Pour vous nouveau bachelier, ou vous qui voulez étudier l'informatique, le moment est venu pour vous inscrire.</p>
+<p>Rentrée : Janvier 2022</p>
+<p>Remplissez ou imprimez la fiche d'inscription en cliquant <a href="pages/formulaire.html" class="ici">ici</a> et une fois remplie, envoyez-la par e-mail à <a href="mailto:contact@hei.school"> contact@hei.school</a>, ou venez la déposer directement chez nous à Ivandry.</p>
+  </div>
+  <div class="col-lg-5 col-md-4 col-sm-4 col-xs-10">
+    <h4 class="parcours"> Inscription & Frais de scolarités</h4>
+    <p>Les frais de scolarités sont de 1.600.000 Ar si payé en une seule fois, à la rentrée et 1.800.000 Ar payable sur 9 mois.</p>
+    <p>Alignée à ses valeurs d'entraide et d'autonomie, HEI va octroyer une Bourse d'excellence à la hauteur de 8 900£ soit 40.000.000 Ar pour le/la major de promotion afin de poursuivre ses études en France.</p>
+    <p>De plus seront exonérés de frais de scolarités de la 1 ère année d'étude les 3 étudiants les mieux classés au concours d'entrée et 2 étudiants suivant des critères sociaux.</p>
+  </div>
+</div>
+</div>
+
+</section>
+<!-- work section --> 
+
+<!-- footer section -->
+<footer id="contact" class="footer">
+  <div class="container">
+    <div class="col-md-4">
+      <h4>Contact</h4>
+      <p> Lot II J 161 R Ambohijatovo Ivandry - Antananarivo.<br>
+        Call: +261 34 94 041 16 <br>
+        Email : <a href="mailto:contact@hei.school"> contact@hei.school </a> <br>
+        Site Web <a href="www.hei.school"> www.hei.school </a></p>
+    </div>
+    <div class="col-md-3">
+      <h4>Présence sociaux</h4>
+      <ul class="footer-share">
+        <li>
+         <p><i style="color: rgb(119, 185, 230);" class="fa fa-facebook"></i>  HEI - Haute Ecole d'Informatique</p>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-5">
+      <p>© 2022 All rights reserved. All Rights Reserved<br>
+        Made with <i style="color: red;" class="fa fa-heart pulse"></i> by Onitsiky</p>
+    </div>
+  </div>
+</footer>
+<!-- footer section --> 
+
+<!-- JS FILES --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script> 
+<script src="js/retina.min.js"></script> 
+<script src="js/modernizr.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
